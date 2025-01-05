efe465f3dd12823428852f742e3f65b55f9216de
Garrett Goon <garrett361@gmail.com>
2025-01-05 18:02:19 +0000

    ssd_minimal_discrete_alt working

--- mamba_ssm/modules/ssd_minimal.py
+++ mamba_ssm/modules/ssd_minimal.py
@@ -98,8 +98,7 @@ def ssd_minimal_no_chunking(X, A, B, C):
 
     return Y
 
-
-def ssd_minimal_discrete_clean(X, A, B, C, block_len):
+def ssd_minimal_discrete_alt(X, A, B, C, block_len):
     """
     Arguments:
         X: (batch, length, n_heads, d_head)
@@ -118,7 +117,6 @@ def ssd_minimal_discrete_clean(X, A, B, C, block_len):
     ]
 
     A = rearrange(A, "b c l h -> b h c l")
-    A_cumsum = A.cumsum( dim=-1)
     A_sum = A.sum(dim=-1)
 
     # 1. Compute the output for each intra-chunk (diagonal blocks)
@@ -126,24 +124,45 @@ def ssd_minimal_discrete_clean(X, A, B, C, block_len):
     Y_diag = torch.einsum("bclgn,bcsgn,bhcls,bcshp->bclhp", C, B, L, X)
 
     # 2. Right-factor
-    T = (A_sum[..., None] - A_cumsum + A).exp()
+    # A_cumsum = A.cumsum( dim=-1)
+    # T_naive = (A_sum[..., None] - A_cumsum).exp()
+    T_mask = torch.triu(
+        torch.ones(block_len, block_len, dtype=bool, device=A.device), diagonal=0
+    )
+    T = A[..., None].masked_fill(T_mask, 0.0).sum(dim=-2).exp()
     right_factor = torch.einsum("bhcl,bclgn,bclhp->bcghnp", T, B, X)
 
-    # 3. Center-factor
-    A_sum_cs = A_sum.cumsum(dim=-1)
-    A_middle = (A_sum_cs[..., None] - A_sum_cs[..., None, :] - A_sum[..., None]).exp()
-    n_chunks = A_middle.shape[-1]
-    mask = torch.triu(torch.ones(n_chunks, n_chunks, dtype=bool, device=A_middle.device), diagonal=0)
-    middle_factor = A_middle.masked_fill(mask, 0.0)
+    # 3. Center-factor. Shape: bhcs, c and s both chunk dims
+    # A_sum_cs = A_sum.cumsum(dim=-1)
+    # center_factor_naive = (A_sum_cs[..., None] - A_sum_cs[..., None, :]).exp()
+
+    # Need a somewhat complicated 3D mask
+    n_chunks = A.shape[-2]
+    center_mask1 = torch.tril(
+        torch.ones(n_chunks, n_chunks, dtype=bool, device=A.device), diagonal=0
+    )
+    center_mask2 = torch.triu(
+        torch.ones(n_chunks, n_chunks, dtype=bool, device=A.device), diagonal=0
+    )
+    center_mask_total = center_mask1[..., None] | center_mask2[..., None, :]
+    center_factor = (
+        A_sum[..., None, None].masked_fill(center_mask_total, 0).sum(dim=-3).exp()
+    )
+    off_diag_mask = torch.triu(
+        torch.ones(n_chunks, n_chunks, dtype=bool, device=A.device), diagonal=0
+    )
+    center_factor = center_factor.masked_fill(off_diag_mask, 0.0)
 
     # 4. Left-factor
-    U = (A_cumsum - A).exp()
+    # U_naive = A_cumsum.exp()
+    # The U_mask ends up being the inverse of the T_mask
+    U = A[..., None].masked_fill(~T_mask, 0).sum(dim=-2).exp()
     left_factor = torch.einsum("bclgn,bhcl->bclghn", C, U)
 
     Y_off = torch.einsum(
         "bclghn,bhcs,bsghnp->bclhp",
         left_factor,
-        middle_factor,
+        center_factor,
         right_factor,
     )
 
