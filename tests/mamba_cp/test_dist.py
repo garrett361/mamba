6f97ce18cb84dc5c162bd82f9a5bc049ed44ad15
Garrett Goon <garrett361@gmail.com>
2025-02-11 19:13:48 +0000

    update dtest

--- tests/mamba_cp/test_dist.py
+++ tests/mamba_cp/test_dist.py
@@ -9,38 +9,33 @@ class TestCausalRingCommsFn(DTest):
         torch.manual_seed(42)
         dim = 16
         t_send = torch.randn(
-            self.get_world_size(),
+            self.world_size,
             dim,
-            device=self.get_device(),
+            device=self.device,
             dtype=torch.bfloat16,
         )
         group = dist.group.WORLD
-        t_recv = RingCommsFn.apply(t_send[self.get_rank()], group)
+        t_recv = RingCommsFn.apply(t_send[self.rank], group)
 
-        torch.testing.assert_close(
-            t_recv, t_send[(self.get_rank() - 1) % self.get_world_size()]
-        )
+        torch.testing.assert_close(t_recv, t_send[(self.rank - 1) % self.world_size])
 
     def test_bwd(self):
         torch.manual_seed(42)
         dim = 16
         t_send = torch.randn(
-            self.get_world_size(),
+            self.world_size,
             dim,
-            device=self.get_device(),
+            device=self.device,
             dtype=torch.bfloat16,
             requires_grad=True,
         )
         group = dist.group.WORLD
-        t_recv = RingCommsFn.apply(t_send[self.get_rank()], group)
+        t_recv = RingCommsFn.apply(t_send[self.rank], group)
         t_recv.pow(2).div(2).sum().backward()
 
-        grad = t_send.grad[self.get_rank()]
-        torch.testing.assert_close(grad, t_send[self.get_rank()])
+        grad = t_send.grad[self.rank]
+        torch.testing.assert_close(grad, t_send[self.rank])
 
-        other_idxs = (
-            torch.arange(self.get_world_size(), device=self.get_device())
-            != self.get_rank()
-        )
+        other_idxs = torch.arange(self.world_size, device=self.device) != self.rank
         zero_grads = t_send.grad[other_idxs]
         torch.testing.assert_close(zero_grads, torch.zeros_like(zero_grads))
