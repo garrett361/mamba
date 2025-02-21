0ffdfefb286096ec65329d281e1ec133b6e18d88
Garrett Goon <garrett361@gmail.com>
2025-02-21 21:01:58 +0000

    real cp test cleanup

--- tests/mamba_cp/test_real_cp.py
+++ tests/mamba_cp/test_real_cp.py
@@ -26,6 +26,7 @@ from mamba_ssm.modules.mamba2_cp import (
 from mamba_ssm.ops.triton.ssd_combined import mamba_chunk_scan_combined
 from mamba_ssm.ops.triton.ssd_combined_cp import (
     mamba_chunk_scan_combined_serial_cp,
+    mamba_chunk_scan_combined_allgather_cp,
 )
 
 
@@ -283,8 +284,6 @@ class _DTestModelBase(DTest):
         ]
         return shard
 
-
-class TestConvCP(_DTestModelBase):
     def get_xBC(self, requires_grad: bool = False) -> torch.Tensor:
         return torch.randn(
             self.batch_size,
@@ -294,6 +293,42 @@ class TestConvCP(_DTestModelBase):
             requires_grad=requires_grad,
         )
 
+    def get_scan_kwargs(self, inputs, mamba2):
+        """
+        Get all kwargs for the scan.
+        """
+        A = -torch.exp(mamba2.A_log.float())  # (nheads) or (d_inner, d_state)
+        _, _, z, xBC, dt = in_proj_split(inputs, mamba2)
+        x, B, C = torch.split(
+            xBC,
+            [
+                mamba2.d_ssm,
+                mamba2.ngroups * mamba2.d_state,
+                mamba2.ngroups * mamba2.d_state,
+            ],
+            dim=-1,
+        )
+
+        scan_kwargs = dict(
+            A=A,
+            chunk_size=mamba2.chunk_size,
+            D=rearrange(mamba2.D, "(h p) -> h p", p=mamba2.headdim)
+            if mamba2.D_has_hdim
+            else mamba2.D,
+            dt_bias=mamba2.dt_bias,
+            dt_softplus=True,
+            x=rearrange(x, "b l (h p) -> b l h p", p=mamba2.headdim),
+            dt=dt,
+            B=rearrange(B, "b l (g n) -> b l g n", g=mamba2.ngroups),
+            C=rearrange(C, "b l (g n) -> b l g n", g=mamba2.ngroups),
+            z=rearrange(z, "b l (h p) -> b l h p", p=mamba2.headdim)
+            if not mamba2.rmsnorm
+            else None,
+        )
+        return scan_kwargs
+
+
+class TestConvCP(_DTestModelBase):
     def test_fwd(self):
         torch.manual_seed(42)
         mamba2 = self.get_mamba2()
@@ -329,40 +364,6 @@ class TestConvCP(_DTestModelBase):
 
 
 class TestSerialCP(_DTestModelBase):
-    def get_scan_kwargs(self, inputs, mamba2):
-        """
-        Get all kwargs for the scan.
-        """
-        A = -torch.exp(mamba2.A_log.float())  # (nheads) or (d_inner, d_state)
-        _, _, z, xBC, dt = in_proj_split(inputs, mamba2)
-        x, B, C = torch.split(
-            xBC,
-            [
-                mamba2.d_ssm,
-                mamba2.ngroups * mamba2.d_state,
-                mamba2.ngroups * mamba2.d_state,
-            ],
-            dim=-1,
-        )
-
-        scan_kwargs = dict(
-            A=A,
-            chunk_size=mamba2.chunk_size,
-            D=rearrange(mamba2.D, "(h p) -> h p", p=mamba2.headdim)
-            if mamba2.D_has_hdim
-            else mamba2.D,
-            dt_bias=mamba2.dt_bias,
-            dt_softplus=True,
-            x=rearrange(x, "b l (h p) -> b l h p", p=mamba2.headdim),
-            dt=dt,
-            B=rearrange(B, "b l (g n) -> b l g n", g=mamba2.ngroups),
-            C=rearrange(C, "b l (g n) -> b l g n", g=mamba2.ngroups),
-            z=rearrange(z, "b l (h p) -> b l h p", p=mamba2.headdim)
-            if not mamba2.rmsnorm
-            else None,
-        )
-        return scan_kwargs
-
     def test_fwd(self):
         torch.manual_seed(42)
         cp_mesh = dist.device_mesh.init_device_mesh("cuda", (self.world_size,))
@@ -393,6 +394,90 @@ class TestSerialCP(_DTestModelBase):
         )
         tol = 1e-3
         torch.testing.assert_close(outputs, outputs_cp_all_gathered, atol=tol, rtol=tol)
+
+    def test_bwd(self):
+        torch.manual_seed(42)
+        cp_mesh = dist.device_mesh.init_device_mesh("cuda", (self.world_size,))
+
+        inputs = self.get_inputs(requires_grad=True)
+        mamba2 = self.get_mamba2()
+
+        inputs_copy = deepcopy(inputs)
+        inputs_cp = self.get_cp_shard(inputs_copy)
+        mamba2_cp = deepcopy(mamba2)
+
+        # Backward on the correct global result
+        kwargs = self.get_scan_kwargs(inputs, mamba2)
+        outputs = mamba_chunk_scan_combined(**kwargs)
+        outputs.sum().backward()
+
+        # And on the CP output:
+        kwargs_cp = self.get_scan_kwargs(inputs_cp, mamba2_cp)
+        outputs_cp = mamba_chunk_scan_combined_serial_cp(cp_mesh=cp_mesh, **kwargs_cp)
+        outputs_cp.sum().backward()
+
+        # Rearrange grads to compare proper slices
+        inputs_grad_shard = self.get_cp_shard(inputs.grad)
+        inputs_cp_grad_shard = self.get_cp_shard(inputs_copy.grad)
+
+        tol = 1e-3
+        torch.testing.assert_close(
+            inputs_grad_shard, inputs_cp_grad_shard, atol=tol, rtol=tol
+        )
+
+        # Parameter grads should match after all-reducing.
+        grads = {n: p.grad for n, p in mamba2.named_parameters() if p.grad is not None}
+        grads_cp = {
+            n: deepcopy(p.grad)
+            for n, p in mamba2_cp.named_parameters()
+            if p.grad is not None
+        }
+        for g in grads_cp.values():
+            dist.all_reduce(g)
+        dist.barrier()  # Apparently needed for correctness if running the test under a debugger.
+        assert set(grads) == set(grads_cp)
+        for n, g_cp in grads_cp.items():
+            g = grads[n]
+            try:
+                torch.testing.assert_close(g, g_cp, atol=tol, rtol=tol)
+            except Exception as e:
+                print(f"Failed on {n}")
+                raise e
+
+
+class TestAllGatherCP(_DTestModelBase):
+    def test_fwd(self):
+        torch.manual_seed(42)
+        cp_mesh = dist.device_mesh.init_device_mesh("cuda", (self.world_size,))
+        inputs = self.get_inputs()
+        mamba2 = self.get_mamba2()
+
+        # The correct global outputs:
+        kwargs = self.get_scan_kwargs(inputs, mamba2)
+        outputs = mamba_chunk_scan_combined(**kwargs)
+
+        # And the CP output:
+        inputs_cp_shard = self.get_cp_shard(inputs)
+        cp_kwargs = self.get_scan_kwargs(inputs_cp_shard, mamba2)
+        outputs_cp = mamba_chunk_scan_combined_allgather_cp(
+            cp_mesh=cp_mesh, **cp_kwargs
+        )
+
+        # All-gather and verify correctness
+        outputs_cp_all_gathered = torch.empty(
+            self.world_size,
+            *outputs_cp.shape,
+            dtype=outputs_cp.dtype,
+            device=outputs_cp.device,
+        )
+        dist.all_gather_into_tensor(
+            outputs_cp_all_gathered, outputs_cp, cp_mesh.get_group()
+        )
+        outputs_cp_all_gathered = rearrange(
+            outputs_cp_all_gathered, "r b l ... -> b (r l) ..."
+        )
+        tol = 1e-3
+        # torch.testing.assert_close(outputs, outputs_cp_all_gathered, atol=tol, rtol=tol)
 
     def test_bwd(self):
         torch.manual_seed(42)
