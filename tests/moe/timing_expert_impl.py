b755041cf55470b7cd8ca9f0951bb8268fd8d569
Garrett Goon <goon@ibm.com>
2025-04-24 21:17:37 +0000

    different data on timing/profiler iters

--- tests/moe/timing_expert_impl.py
+++ tests/moe/timing_expert_impl.py
@@ -10,9 +10,9 @@ expert_classes = list(NON_EP_EXPERT_CLASSES.values())
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--warmups", type=int, default=3)
-    parser.add_argument("--reps", type=int, default=16)
-    parser.add_argument("--in_features", type=int, default=3072)
+    parser.add_argument("--warmups", type=int, default=5)
+    parser.add_argument("--reps", type=int, default=32)
+    parser.add_argument("--in_features", type=int, default=4096)
     parser.add_argument("--d_intermediate", type=int, default=1344)
     parser.add_argument("--n_routed_experts", type=int, default=64)
     parser.add_argument("--n_activated_experts", type=int, default=8)
@@ -32,14 +32,19 @@ if __name__ == "__main__":
     cache_size_bytes = 512 * 2**20  # 512k
     cache = torch.empty(int(cache_size_bytes // 4), dtype=torch.int, device="cuda")
 
-    inputs = torch.randn(args.bsz * args.seqlen, args.in_features, **factory_kwargs)
+    iters_per_impl = args.warmups + args.reps
+    inputs = torch.randn(
+        iters_per_impl, args.bsz * args.seqlen, args.in_features, **factory_kwargs
+    )
     weights = torch.randn(
+        iters_per_impl,
         args.bsz * args.seqlen,
         args.n_activated_experts,
         **factory_kwargs,
     )
     indices = (
         torch.randn(
+            iters_per_impl,
             args.bsz * args.seqlen,
             args.n_routed_experts,
             device=device,
@@ -56,19 +61,23 @@ if __name__ == "__main__":
             **factory_kwargs,
         )
 
+        idx = 0
+
         for _ in range(args.warmups):
-            out = model(inputs, weights, indices)
+            out = model(inputs[idx], weights[idx], indices[idx])
             if not args.no_bwd:
                 out.pow(2).sum().backward()
             cache.zero_()
+            idx += 1
 
         timer = CUDATimer()
         for _ in range(args.reps):
             with timer:
-                out = model(inputs, weights, indices)
+                out = model(inputs[idx], weights[idx], indices[idx])
                 if not args.no_bwd:
                     out.pow(2).sum().backward()
             cache.zero_()
+            idx += 1
         time_s = timer.get_mean_time_s()
         time_std_s = timer.get_std_time_s()
         print(
