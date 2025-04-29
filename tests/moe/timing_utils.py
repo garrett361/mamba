015b71eb313eddf8176660a076b5ad31da55d131
Garrett Goon <goon@ibm.com>
2025-04-29 17:34:22 +0000

    profiling_model_dist.py

--- tests/moe/timing_utils.py
+++ tests/moe/timing_utils.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import torch
 import torch.nn as nn
 from torch.distributed import init_device_mesh
@@ -5,6 +6,8 @@ from torch.distributed._composable.fsdp import fully_shard
 from torch.distributed.device_mesh import DeviceMesh, init_device_mesh
 from torch.distributed.fsdp import MixedPrecisionPolicy
 from torch.profiler import record_function
+from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from mamba_ssm.modules.moe import MoE
 
 
 class CUDATimer:
@@ -131,3 +134,61 @@ def shard_sequential_model(
     # bwd
     seq_model.layers[0].set_reshard_after_backward(False)
     fully_shard(seq_model, mesh=fsdp_mesh, mp_policy=mp_policy)
+
+def shard_full_model(
+    model: MambaLMHeadModel,
+    ep_degree: int,
+    world_size: int,
+    ep_mesh: DeviceMesh,
+    fsdp_mesh: DeviceMesh,
+    mp_policy: MixedPrecisionPolicy,
+    rank: Optional[int]=None
+) -> None:
+    fully_shard(model.lm_head, mesh=fsdp_mesh, mp_policy=mp_policy)
+    fully_shard(
+        model.backbone.embedding, mesh=fsdp_mesh, mp_policy=mp_policy
+    )
+    # NOTE: @goon - model.backbone.layers is a module_dict on the MoE branch
+    for idx, block in model.backbone.layers.items():
+        # Cases:
+        # 1. ep_degree = 1: full replication, fully shard with the fsdp_mesh
+        # 2. ep_degree = world_size: no expert replication at all. Ignore experts in fully_shard
+        # 3. world_size > ep_degree > world_size: world_size // ep_degree expert replicas.
+
+        # The ignored_params arg requires torch nightly (> 2.6.0)
+        ignored_params = set()
+        if isinstance(block.mlp, MoE):
+            if ep_degree == 1:
+                if not rank:
+                    print("Not sharding MoE block")
+                pass
+            elif ep_degree == world_size:
+                # No replication in this case.
+                if not rank:
+                    print(f"Ignoring params for {block.mlp.experts=}")
+                ignored_params.add(block.mlp.experts.parameters())
+            else:
+                # Don't reshard due to comms costs
+                if not rank:
+                    print(f"Wrapping {block.mlp.experts=}")
+                fully_shard(
+                    block.mlp.experts,
+                    mesh=ep_mesh["outer"],
+                    mp_policy=mp_policy,
+                    reshard_after_forward=False,
+                )
+                block.mlp.experts.set_reshard_after_backward(False)
+        is_not_last_block = int(idx) < len(model.backbone.layers) - 1
+        fully_shard(
+            block,
+            mesh=fsdp_mesh,
+            ignored_params=ignored_params,
+            mp_policy=mp_policy,
+            reshard_after_forward=is_not_last_block,
+        )
+    fully_shard(
+        model,
+        mesh=fsdp_mesh,
+        reshard_after_forward=False,
+        mp_policy=mp_policy,
+    )
