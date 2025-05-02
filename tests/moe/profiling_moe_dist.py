5a089133990ca826b4b642c2f9eaac45fccdb0a5
Garrett Goon <goon@ibm.com>
2025-05-02 13:28:27 +0000

    missing import fix

--- tests/moe/profiling_moe_dist.py
+++ tests/moe/profiling_moe_dist.py
@@ -2,7 +2,7 @@ import json
 import os
 import subprocess
 from argparse import ArgumentParser
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import torch
